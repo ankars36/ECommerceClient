410c1ee190d9c9c0c7d3d968acf15a4f0de2f8cc
ankars36 <e.yildiz162@gmail.com>
2024-01-08 02:32:55 +0100

    Delete Directive İle Silme Operasyonu

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -18,7 +18,7 @@ export class ListComponent extends BaseComponent implements OnInit {
     super(spinner)
   }
 
-  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate'];
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate', 'edit', 'delete'];
   dataSource: MatTableDataSource<any> = null;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -39,8 +39,8 @@ export class ListComponent extends BaseComponent implements OnInit {
     await this.getProducts();
   }
 
-  async pageChanged(){
-    await this.getProducts(); 
+  async pageChanged() {
+    await this.getProducts();
   }
 
 }
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClientService } from '../http-client.service';
 import { Create_Product } from '../../../contracts/create_product';
 import { HttpErrorResponse } from '@angular/common/http';
 import { List_Product } from '../../../contracts/list_product';
+import { Observable, firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -29,8 +30,8 @@ export class ProductService {
       });
   }
 
-  async read(page: number = 0, size: number = 5, successCallBack?: () => void, errorCallback?: (errorMessage: string) => void): Promise<{totalCount: number; products: List_Product[]}> {
-    const promisedData: Promise<{totalCount: number; products: List_Product[]}> = this.httpClientService.get<{totalCount: number; products: List_Product[]}> ({
+  async read(page: number = 0, size: number = 5, successCallBack?: () => void, errorCallback?: (errorMessage: string) => void): Promise<{ totalCount: number; products: List_Product[] }> {
+    const promisedData: Promise<{ totalCount: number; products: List_Product[] }> = this.httpClientService.get<{ totalCount: number; products: List_Product[] }>({
       controller: "products",
       queryParameters: `page=${page}&size=${size}`
     }).toPromise();
@@ -39,5 +40,12 @@ export class ProductService {
       .catch((errorResponse: HttpErrorResponse) => errorCallback(errorResponse.message))
 
     return await promisedData;
+  }
+
+  async delete(id: string) {
+    const deleteObservable: Observable<any> = this.httpClientService.delete<any>({
+      controller: "products"
+    }, id);
+    await firstValueFrom(deleteObservable);
   }
 }
