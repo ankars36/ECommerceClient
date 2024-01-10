83b852e61ba7e22be9070abae06cacdc2903afa8
ankars36 <e.yildiz162@gmail.com>
2024-01-10 23:36:22 +0100

    Diyalog Sayfalarının Merkezileştirilmesi 2

--- src/app/dialogs/delete-dialog/delete-dialog.component.ts
+++ src/app/dialogs/delete-dialog/delete-dialog.component.ts
@@ -1,11 +1,26 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { BaseDialog } from '../base/base-dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-delete-dialog',
   templateUrl: './delete-dialog.component.html',
-  styleUrl: './delete-dialog.component.scss'
+  styleUrl: './delete-dialog.component.scss',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
 })
 export class DeleteDialogComponent extends BaseDialog<DeleteDialogComponent> {
   constructor(
--- src/app/dialogs/file-upload-dialog/file-upload-dialog/file-upload-dialog.component.ts
+++ src/app/dialogs/file-upload-dialog/file-upload-dialog/file-upload-dialog.component.ts
@@ -1,11 +1,26 @@
 import { Component, Inject } from '@angular/core';
 import { BaseDialog } from '../../base/base-dialog';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-file-upload-dialog',
   templateUrl: './file-upload-dialog.component.html',
-  styleUrl: './file-upload-dialog.component.scss'
+  styleUrl: './file-upload-dialog.component.scss',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
 })
 export class FileUploadDialogComponent extends BaseDialog<FileUploadDialogComponent> {
   constructor(
