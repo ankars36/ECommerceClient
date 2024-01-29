276cfe7e4fceb531b230c48ed1c8471ef397a28a
ankars36 <e.yildiz162@gmail.com>
2024-01-30 00:23:25 +0100

    Siparişlerin Yönetim Panelinde Listelenmesi

--- src/app/admin/components/orders/orders.module.ts
+++ src/app/admin/components/orders/orders.module.ts
@@ -2,18 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { DialogModule } from '@angular/cdk/dialog';
+import { FileUploadModule } from '../../../services/common/file-upload/file-upload.module';
+import { DeleteDirectiveModule } from '../../../directives/admin/delete-directive.module';
 
 
 
 @NgModule({
   declarations: [
-    OrdersComponent
+    OrdersComponent,
+    ListComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {path:"",component:OrdersComponent}
-    ])
+    ]),
+    MatSidenavModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatTableModule, MatPaginatorModule,
+    DialogModule,
+    FileUploadModule,
+    DeleteDirectiveModule
   ]
 })
 export class OrdersModule { }
